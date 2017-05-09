0c35c0e890de35002a3d2ba72e0372fd71660cf7
Thomas Keating <twkeating1@gmail.com>
2017-05-09 09:28:34 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Tour of Heroes';
+	hero = 'Windstorm';
+	name = 'Angular'; }
